f90c2cb2e6e5e14c084c13f7eefb978fbabc05b2
Vincent Nguyen <mr.vannguyen92@gmail.com>
2019-09-11 13:44:12 +0700

    Add mongo db

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import * as express from 'express';
+import * as http from 'http';
 import * as cors from 'cors';
+import * as mongoose from 'mongoose';
+
 import { router } from './router';
 import { requestLoggerMiddleware } from './middlewares/requestLoggerMiddleware';
 
 class Application {
   app: express.Application;
+  PORT = 8080;
+  MONGO_URI = 'mongodb://localhost:27017/demo';
 
   constructor() {
     this.app = express();
@@ -15,7 +20,7 @@ class Application {
   }
 
   settings() {
-    this.app.set('port', 8080);
+    this.app.set('port', this.PORT);
     this.app.disable('x-powered-by');
   }
 
@@ -33,8 +38,19 @@ class Application {
   }
 
   start() {
-    this.app.listen(this.app.get('port'), () => {
-      console.log(`Server is running at ${this.app.get('port')  }`);
+    const server = http.createServer(this.app);
+
+    server.listen(this.app.get('port'));
+    server.on('listening', () => {
+      console.log(`Server is running at ${this.app.get('port')}`);
+
+      mongoose.connect(this.MONGO_URI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true});
+      mongoose.connection.on('open', () => {
+        console.info('Connected to Mongo.');
+      });
+      mongoose.connection.on('error', (err: any) => {
+        console.error(err);
+      });
     });
   }
 }
