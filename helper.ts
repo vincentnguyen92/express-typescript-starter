d55c8071b78760bc867d79e67b5a8669e7d08436
Vincent Nguyen <mr.vannguyen92@gmail.com>
2019-09-16 17:46:15 +0700

    Remove s tring at end Controllers directory

--- helper.ts
+++ helper.ts
@@ -5,7 +5,7 @@
  */
 function Controller(str: String) {
   const arrInput = str.split('@');
-  const objController = require(`./src/Controllers/${arrInput[0]}`).default;
+  const objController = require(`./src/Controller/${arrInput[0]}`).default;
   const funController = new objController();
   
   return funController[arrInput[1]];
